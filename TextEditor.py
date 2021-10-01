f1af18b8a5a4ba7ba29e4201812558fe93220b67
Akshay Prakash <51133712+aprakash7@users.noreply.github.com>
2021-10-01 11:34:54 -0700

    removed unncessary comments

--- TextEditor.py
+++ TextEditor.py
@@ -5,12 +5,12 @@ filename = None
 def newFile():
     global filename #Using the global varibale
     filename = "Untitled" #Because whenever you create a new file it is untitled
-    text.delete(0.0, END)    #we're creating our main textbox for entering the text, by creating a variable called text
-    #row and column, 0.0
+    text.delete(0.0, END)   
+   
 def saveFile():
     global filename
-    t = text.get(0.0, END) # Going to get all the text
-    f = open(filename, 'w') #end the modew w for write
+    t = text.get(0.0, END) #get
+    f = open(filename, 'w') #write into
     f.write(t)
     f.close
 
@@ -18,15 +18,15 @@ def saveFile():
     #And then opens file with the file name
 
 def saveAs():
-    f = asksaveasfile(mode = 'w', defaultextension = '.txt')#Existing in the tkinter file dialog
-    t = text.get(0.0, END)  # Going to get all the text
-    try: #try and except statement
+    f = asksaveasfile(mode = 'w', defaultextension = '.txt')
+    t = text.get(0.0, END)  
+    try: #try except
         f.write(t.rstrip()) #Cuts off the whitespace underneath the text that we write into the file
     except:
         showerror(title= 'Oops!', message = "Unable to save file..") #showerror is a function of tkinter
 
 def openfile():
-    f = askopenfile(mode = 'r') #r for read
+    f = askopenfile(mode = 'r') #read
     t = f.read()
     text.delete(0.0, END) #get rid of all the text in the textbox
     text.insert(0.0, t)
@@ -37,7 +37,7 @@ root.minsize(width = 400, height = 400)
 root.maxsize(width = 400, height = 400)
 
 text = Text(root, width = 400, height= 400) #creating textbox finally
-text.pack() #Display textbox
+text.pack() 
 
 #Creating menu  bar
 menubar = Menu(root)
